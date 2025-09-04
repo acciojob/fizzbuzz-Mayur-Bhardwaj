08267e72aea6539269b8646c9fdaccbc3956f9df
softo-mayur <mayur.bhardwaj@softomation.com>
2025-09-04 20:44:35 +0530

    Add the FizzBuzz Logic

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
+
+//your JS code here. If required.
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    alert("FizzBuzz");
+  } else if (i % 3 === 0) {
+    alert("Fizz");
+  } else if (i % 5 === 0) {
+    alert("Buzz");
+  } else {
+    alert(i);
+  }
+} 
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
